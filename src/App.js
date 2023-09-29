652cdc6127d515d512993dd3f190cfdc4614ee83
goish2468 <goish2468@gmail.com>
2023-09-29 16:25:16 +0800

    font-family

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Table, Button, Layout, Input } from "antd";
+import { Table, Button, Layout, Input, ConfigProvider } from "antd";
 import React, { useState, useRef } from "react";
 import ReactToPrint from "react-to-print";
 import { PrinterOutlined } from "@ant-design/icons";
@@ -46,9 +46,8 @@ function App() {
 
   const handleDelete = (key) => {
     const newData = dataSource.filter((item) => item.key !== key);
-    
+
     setDataSource(newData);
-    
   };
 
   const onChange = (e) => {
@@ -79,58 +78,67 @@ function App() {
 
   return (
     <div className="App">
-      <Layout ref={(el) => (componentRef = el)}>
-        <Header
-          style={{
-            color: "white",
-            textAlign: "center",
-          }}
-        >
-          桌球出賽名單
-        </Header>
-        <Content className="site-layout" style={{ padding: "0 50px" }}>
-          <div style={{ padding: 24, height: "100%" }}>
-            <div style={{ display: "block", textAlign: "right" }}>
-              <ReactToPrint
-                trigger={() => (
-                  <Button icon={<PrinterOutlined />}>Print</Button>
-                )}
-                content={() => componentRef}
-              />
-            </div>
+      <ConfigProvider
+        theme={{
+          token: {
+            fontFamily: "fantasy",
+            fontSize: 20,
+          },
+        }}
+      >
+        <Layout ref={(el) => (componentRef = el)}>
+          <Header
+            style={{
+              color: "white",
+              textAlign: "center",
+            }}
+          >
+            Table tennis list
+          </Header>
+          <Content className="site-layout" style={{ padding: "0 50px" }}>
+            <div style={{ padding: 24, height: "100%" }}>
+              <div style={{ display: "block", textAlign: "right" }}>
+                <ReactToPrint
+                  trigger={() => (
+                    <Button icon={<PrinterOutlined />}>Print</Button>
+                  )}
+                  content={() => componentRef}
+                />
+              </div>
 
-            <div style={{ width: "100%"}}>
-              <div style={{ display: "inline-block" }}>隊名:&nbsp;</div>
-              <div style={{ display: "inline-block" }}>
-                <Input showCount maxLength={20} onChange={onChange} />
+              <div style={{ width: "100%" }}>
+                <div style={{ display: "inline-block" }}>Team Name:&nbsp;</div>
+                <div style={{ display: "inline-block" }}>
+                  <Input showCount maxLength={20} onChange={onChange} />
+                </div>
+                <br />
+                <br />
+                <div style={{ display: "inline-block" }}>Team Lead:&nbsp;</div>
+                <div style={{ display: "inline-block" }}>
+                  <Input showCount maxLength={20} onChange={onChange} />
+                </div>
+                <br />
+                <br />
+                <Button type="primary" onClick={handleAdd}>
+                  +1 Player
+                </Button>
+                <Button style={{ marginLeft: 6 }} onClick={handleAdd2}>
+                  +2 Player
+                </Button>
               </div>
               <br />
-              <br />
-              <div style={{ display: "inline-block" }}>隊長:&nbsp;</div>
-              <div style={{ display: "inline-block" }}>
-                <Input showCount maxLength={20} onChange={onChange} />
-              </div>
-              <br />
-              <br />
-              <Button type="primary" onClick={handleAdd}>
-                +1 Player
-              </Button>
-              <Button style={{ marginLeft: 6 }} onClick={handleAdd2}>
-                +2 Player
-              </Button>
+              <Table columns={columns} dataSource={dataSource} />
             </div>
-            <br />
-            <Table columns={columns} dataSource={dataSource} />
-          </div>
-        </Content>
-        <Footer
-          style={{
-            textAlign: "center",
-          }}
-        >
-          桌球王 ©2023 Created by 阿瑜
-        </Footer>
-      </Layout>
+          </Content>
+          <Footer
+            style={{
+              textAlign: "center",
+            }}
+          >
+            桌球王 ©2023 Created by 阿瑜
+          </Footer>
+        </Layout>
+      </ConfigProvider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,7 +11,7 @@ root.render(
     <App />
   </React.StrictMode>,
 );
-
+// do change to github page 1,2
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
@@ -21,5 +21,3 @@ reportWebVitals();
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
-
-
